8df4deba6b8d11b6bf361b9c10d6b5e9f8b9bedd
daljeet05 <daljeetsingh1990@gmail.com>
2018-12-11 11:19:42 +0530

    web_route_files

--- admin-panel-laravel/routes/web.php
+++ admin-panel-laravel/routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/dashboard', 'HomeController@index')->name('dashboard');
+//Route::get('/dashboard', 'HomeController@index')->name('dashboard');
